e27670d38db8ce232d1b246522bf4f1dec605d52
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-27 23:20:37 +0530

    Add files via upload

---
+++ 01Basic/celsius.js
@@ -0,0 +1,20 @@
+var score = 100
+
+score = 130 
+
+const bonus = 50 
+
+var finalScore = (score + bonus) * 0.8
+
+var complex = ((4 + 4) * 5) / 2
+
+//console.log(finalScore)
+//console.log(complex)
+
+let tempInFahrem=nheit = 32
+
+//do calculas
+
+let celsius = (tempInFahrem - 32) * 5/9
+
+console.log(celsius)
