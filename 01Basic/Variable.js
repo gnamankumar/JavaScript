8bf4433ff1a0fc0b552964e9dc927f80008be0a8
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-27 22:54:22 +0530

    variable and constant

---
+++ 01Basic/Variable.js
@@ -0,0 +1,22 @@
+/* Variable is use the -> let 
+Constant is use -> const
+*/
+let name = 'Naman'
+let Name = 'Kumar'
+
+console.log(name + ' ' + Name)
+
+
+let score = 100
+let bonus = 20
+
+let TotalScore = score + bonus
+
+
+console.log(TotalScore)
+
+
+let firstName = 'Naman'
+let lastName = 'Goel'
+
+console.log(firstName +' ' + lastName)
