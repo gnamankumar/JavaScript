45ac414da2ddc1506d26b3f1320ced0a8eac87a0
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-28 00:00:26 +0530

    Add files via upload

---
+++ 02Basic/learnifElse.js
@@ -0,0 +1,31 @@
+/*
+if (false){
+    console.log('I am inside if Block')
+    
+} else if (true){
+
+    console.log('I am inside ELSE IF Block')
+}
+ else {
+    console.log('I am inside else Block')
+}
+*/
+
+
+var whoIsHere = ''
+
+if (whoIsHere === 'user') {
+    console.log('Greeting message for user')
+    console.log('Allow acces to view all cousrses')
+    
+} else if (whoIsHere === 'teacher'){
+    console.log('Greeting message for TEACHER')
+    console.log('Allow acces to his courses')
+
+}else{
+    console.log('MESSAGE: please verify your email')
+    console.log('Send user an email for verification')
+
+}
+
+
