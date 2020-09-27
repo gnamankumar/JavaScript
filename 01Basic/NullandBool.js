302e6cdafaa1b5b6eaf37962ed9ac22e3c9e4ff9
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-27 23:38:29 +0530

    Boolen use

---
+++ 01Basic/NullandBool.js
@@ -0,0 +1,24 @@
+//temp is coming from website API
+
+let temp = 0
+
+//done some calculation
+//temp =  2
+
+console.log("Current temprature is:" + temp)
+
+
+//Grade system
+
+//10 - Great job
+// 5 - work harder
+// 2 - poor
+// 0 - fail
+
+//Boolean - True/False
+
+let actualMarks = 10
+
+let myGrade = (actualMarks <=10)
+
+console.log(myGrade)
