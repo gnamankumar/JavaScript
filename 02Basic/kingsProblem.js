4c34f9ff656b91c98d4b82a2c1103c7973405129
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-28 22:46:29 +0530

    This is a Problem of kings Territory

---
+++ 02Basic/kingsProblem.js
@@ -0,0 +1,17 @@
+//Kings Problem
+
+let king = 'John'
+
+if (true){
+   // let king = 'Sam'
+
+    if(true){
+        let king = 'Ram'
+        console.log(king);
+
+    }
+}
+
+if(true){
+    console.log("I am second Part : " + king);
+}
