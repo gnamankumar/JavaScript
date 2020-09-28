c70935d304cab4897ccc7b9c0da409386c92ff09
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-28 23:09:04 +0530

    Array Use

---
+++ 02Basic/marvels.js
@@ -0,0 +1,9 @@
+//Datatype - Array
+
+const superHeroes = ['Iron Man','Spiderman','Capt. America','Thor']
+
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[superHeroes.length -1]);
+
+console.log(`We have ${superHeroes.length} Super Heros`);
