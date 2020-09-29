8c2dc1eac590dd62fd09bb9c28b80cec9290ea83
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-29 18:40:22 +0530

    Use PUSH POP SPLICE

---
+++ 02Basic/TypeArray.js
@@ -0,0 +1,28 @@
+const numbers = ['One','Two','Three','Four','Five','Six']
+
+//numbers[1] = 'SOMETHING'
+
+//console.log(numbers);
+
+//start 
+
+//console.log(numbers.shift());
+//console.log(numbers);
+
+//numbers.unshift('SOMETHING')
+//console.log(numbers);
+
+//end
+
+//numbers.pop()
+//console.log("The element to be deleted is : " + numbers.pop())
+//console.log(numbers);
+
+numbers.push('Seven') 
+console.log(numbers);
+
+
+//Middle
+
+numbers.splice(2,1,'SOMETHING')
+console.log(numbers);
