d442480be6a090238248f8ba5dce7a2c0e9695ee
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-29 19:36:56 +0530

    Create Todo List using ForEach function and For Loop

---
+++ 01Intermediate/TodoList.js
@@ -0,0 +1,17 @@
+/*const myTodos = []
+
+myTodos.push('Buy Bread')
+myTodos.push('Drink Milk')
+myTodos.push('Go to Gym')
+
+myTodos.forEach(function(todo, index){
+    console.log(`Yor task no. ${index +1} is: ${todo}`);
+
+}) */
+
+const myTodos = ['Buy Bread','Drink Milk','Go to Gym']
+
+for (let i=0; i<myTodos.length; i++){
+    console.log(myTodos);
+    
+}
