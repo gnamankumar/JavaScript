e46ce02a3d848810264ab915a41974f958d75f24
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-09-30 18:13:04 +0530

    Create Objects

---
+++ 02Intermediate/myObject.js
@@ -0,0 +1,11 @@
+let myYoutubeVideo = {
+    title: 'Loops in javascript',
+    videoLength: '15',
+    videoCreator: 'Naman Kumar',
+    videoDescription: 'This is a video description and a long one'
+    
+}
+
+console.log(myYoutubeVideo);
+
+console.log(`Hey new Video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}` );
